21398643cba836b9f4be0b037fe51171fae8d770
Eva Stenberg <evastenberg01@gmai.com>
2023-05-01 13:15:01 -0700

    added path from navbar to dorm pages

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,77 +21,66 @@ function Navbar() {
                         <a href="#">Dorms <i class="fas fa-caret-down"></i></a>
                         <ul class="sub-menu">
                             <li>
-                                <a href="#">Swig Hall <i class="fas fa-caret-down"></i></a>
+                                <a href="/dorms/swig-hall">Swig Hall <i class="fas fa-caret-down"></i></a>
                                 <ul class="sub-menu">
-                                        <li>
-                                            <a href="#">Lobby</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Dorm Room</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Floor Lounge</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Sky Lounge</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Basement</a>
-                                        </li>
+                                    <li>
+                                        <a href="/dorms/swig-hall/lobby">Lobby</a>
+                                    </li>
+                                    <li>    
+                                        <a href="/dorms/swig-hall/dorm-room">Dorm Room</a>
+                                    </li>
+                                    <li>
+                                        <a href="/dorms/swig-hall/floor-lounge">Floor Lounge</a>
+                                    </li>
+                                    <li>
+                                        <a href="/dorms/swig-hall/sky-lounge">Sky Lounge</a>
+                                    </li>
+                                    <li>
+                                        <a href="/dorms/swig-hall/basement">Basement</a>
+                                    </li>
                                 </ul>
                             </li>
+
                             <li>
-                            <a href="#">Graham Hall <i class="fas fa-caret-down"></i></a>
-                                <ul class="sub-menu">
+                                <li class="menu-item-has-children">
+                                    <a href="/dorms/graham-hall">Graham Hall <i class="fas fa-caret-down"></i></a>
+                                    <ul class="sub-menu">
                                 <li>
-                                        <a href="https://my.treedis.com/tour/graham-lobby">Lobby</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Lounge 1st Floor</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Lounge</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Movie Room</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Dorm Room</a>
-                                    </li>
-                                </ul> 
-                            </li>
-                            <li>
-                            <a href="#">Campisi Hall <i class="fas fa-caret-down"></i></a>
-                                <ul class="sub-menu">
+                                    <a href="https://my.treedis.com/tour/graham-lobby">Lobby</a>
+                                </li>
                                 <li>
-                                        <a href="#">Lobby</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Lounge</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Dorm Room</a>
-                                    </li>
-                                </ul> 
-                            </li>
-                            <li>
-                            <a href="#">Finn Hall <i class="fas fa-caret-down"></i></a>
+                                    <a href="/dorms/graham-hall/lounge-1st-floor">Lounge 1st Floor</a>
+                                </li>
+                                <li>
+                                    <a href="/dorms/graham-hall/lounge">Lounge</a>
+                                </li>
+                                <li>
+                                    <a href="/dorms/graham-hall/movie-room">Movie Room</a>
+                                </li>
+                                <li>
+                                    <a href="/dorms/graham-hall/dorm-room">Dorm Room</a>
+                                </li>
+                            </ul>
+                        </li>
+                            <li class="menu-item-has-children">
+                                <a href="/dorms/campisi-hall">Campisi Hall <i class="fas fa-caret-down"></i></a>
                                 <ul class="sub-menu">
-                                    <li>
-                                        <a href="#">Lobby</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Kitchen</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Study Room</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Dorm Room</a>
-                                    </li>
-                                </ul> 
+                                    <li><a href="/dorms/campisi-hall/lobby">Lobby</a></li>
+                                    <li><a href="/dorms/campisi-hall/lounge">Lounge</a></li>
+                                    <li><a href="/dorms/campisi-hall/dorm-room">Dorm Room</a></li>
+                                </ul>
                             </li>
-                            <li>
+
+                            <li class="menu-item-has-children">
+                                <a href="/dorms/finn-hall">Finn Hall <i class="fas fa-caret-down"></i></a>
+                                <ul class="sub-menu">
+                                    <li><a href="/dorms/finn-hall/lobby">Lobby</a></li>
+                                    <li><a href="/dorms/finn-hall/kitchen">Kitchen </a></li>
+                                    <li><a href="/dorms/finn-hall/study-room">Study Room</a></li>
+                                    <li><a href="/dorms/finn-hall/dorm-room">Dorm Room</a></li>
+                                </ul>
+                            </li>
+
                             <a href="#">Dunne Hall <i class="fas fa-caret-down"></i></a>
                                 <ul class="sub-menu">
                                     <li>
