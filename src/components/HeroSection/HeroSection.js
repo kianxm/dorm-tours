d500050ddcbc01b23b76c6116d74559279096adb
Kian Malakooti <kianjmalakooti@gmail.com>
2023-05-01 11:21:54 -0700

    Changed hero section image

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './HeroSection.css'
 import '../../App.css'
-import heroVideo from '../../videos/hero-video.mp4'
+// import heroVideo from '../../videos/hero-video.mp4'
 import { Button } from '../Button/Button';
+import swig from '../../images/dorm_images/pics/swig.jpg'
 
 function HeroSection() {
     return (
         <div className="hero-container">
-          <video src={heroVideo} autoPlay loop muted />
+          {/* <video src={heroVideo} autoPlay loop muted /> */}
+          <img className='hero-image' src={swig} alt='Swig'/>
             <h1>Discover Your Dream Dorm Today</h1>
             <p>Explore our top-rated dorms and find the perfect place to call home during your college years.<br/>With state-of-the-art amenities and a prime location on campus,<br/>you won't want to live anywhere else.</p>
             <div className='hero-btns'>
